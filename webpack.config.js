99eb7a443b5403309dce3ff807a1d49d82f3664e
Nata Malinova <rawr@raery.ru>
2021-03-29 23:36:01 +0700

    publicPath to webpack.config js added

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     output: { //output содержит имя файла сборки (filename) и путь до папки, куда этот файл будет сохранен (path)
         path: path.resolve(__dirname, "static", "build"), //__dirname — это константа node.js, определяемая автоматически в каждом файле и содержащая путь до папки, в которой лежит исполняемый файл (у нас это корневая папка)
         filename: 'app.js',
+        publicPath: ''
     },
     plugins: [
         new HtmlWebpackPlugin({
