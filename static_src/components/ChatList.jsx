3573888236378a7ea1f971a1ec07c68d48588d3e
Nata Malinova <rawr@raery.ru>
2021-04-09 01:20:07 +0700

    some bugs fixed

--- static_src/components/ChatList.jsx
+++ static_src/components/ChatList.jsx
@@ -7,18 +7,18 @@ const ChatList = (props) => {
 
     return (
         <div className="chatlist">
-            {props.list.map((item, index) => (
-                <Link to={`/chats/${index}`}
-                    className={`chat-item${(parseInt(activeChat) === index) ? ' _active' : ''}`}
-                    key={index}
-                >
-                    {item}
-                </Link>
-            ))}
+            <div className="chatlist-wrapper">
+                {props.list.map((item, index) => (
+                    <Link to={`/chats/${index}`}
+                        className={`chat-item${(parseInt(activeChat) === index) ? ' _active' : ''}`}
+                        key={index}
+                    >
+                        {item}
+                    </Link>
+                ))}
+            </div>
             <button className="button">Добавить чат</button>
         </div>
-
-
     )
 }
 export default ChatList
